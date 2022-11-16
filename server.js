e72998aa143d9d97d55ba566fdba415bda8255c7
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-11-15 21:43:13 -0500

    index and show route working

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 // Dependencies
+require("dotenv").config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const methodOverride = require('method-override')
 const PORT = process.env.PORT || 3000
+
+// Middleware
+app.use(morgan("tiny"))
+app.use(methodOverride("_method"))
 
 // Database
 const pokemons = require('./models/pokemon.js')
 
-// Get Route
+// Index Route
 app.get('/pokemon', (req, res) => {
-    res.send("Working!")
+    res.render("index.ejs", {
+        pokemons
+    })
+})
+
+// New Route
+app.get('/pokemon/new', (req, res) => {
+    res.render("new.ejs")
+})
+
+// Show Route
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', {
+        pokemon: pokemons[req.params.id],
+        index: req.params.id
+    })
 })
 
 // Server Listening
