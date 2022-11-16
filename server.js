d6d051c987003b55a1b5e7913a7419a56e99d5ff
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-11-16 14:29:29 -0500

    edit, delete, update routes created

--- server.js
+++ server.js
@@ -25,6 +25,32 @@ app.get('/pokemon/new', (req, res) => {
     res.render("new.ejs")
 })
 
+// Create Route
+app.post('/pokemon', (req, res) => {
+    pokemons.push(req.body)
+    res.redirect("/pokemon")
+})
+
+// Edit Route
+app.get("/pokemon/:id/edit", (req, res) => {
+    res.render("edit.ejs", {
+        pokemon: pokemons[req.params.id],
+        index: req.params.id
+    })
+})
+
+// Update Route
+app.put("/pokemon/:id", (req, res) => {
+    pokemons[req.params.id] = req.body
+    res.redirect("/pokemon")
+})
+
+// Delete Route
+app.delete("/pokemon/:id", (req, res) => {
+    pokemons.splice(req.params.id, 1)
+    res.redirect("/pokemon")
+})
+
 // Show Route
 app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', {
